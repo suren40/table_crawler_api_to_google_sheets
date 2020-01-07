af1b4ba6bfd78d9853539db3c10b30abc583514b
suren40 <72bel240@wrc.edu.np>
2020-01-07 11:52:38 +0545

    added the comments of the file

--- json_manipulation.py
+++ json_manipulation.py
@@ -1,25 +1,44 @@
 #!/usr/bin/env python3
 """
-This is for manipulation of data.
+This is the python script used in my lambda function to recieve the json file sent from API-Gateway of AWS and sends 
+that data to the google sheets.
+--
+What is does?
+* Receives the json file and send to the google sheets.
+---
+Modules used:
+* gspread
+* oauth2client
+* json,string
+
+
+How API is received?
 
 """
+
+
 import string
 import json
-
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+# It is like gives the access to the google apis for this projectes
 SCOPES = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
           "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+# It contains the crendentials for authencation of our file.
+# For example I rename it as auth.json
+JSON_FILE_FOR_AUTHENTICATION = "auth.json"
+# we have to define the function with the parameter event and context. where context carries our configuration of lambda
 
 def handler(event,context):
-    creds = ServiceAccountCredentials.from_json_keyfile_name("auth.json", SCOPES)
+# event receives our data and loads in our body
+    creds = ServiceAccountCredentials.from_json_keyfile_name(JSON_FILE_FOR_AUTHENTICAION, SCOPES)
     client = gspread.authorize(creds)
     body = json.loads(event['body'])
     print(body)
     start = body['begin']
     sheetID = body['sheet_id']
     if len(sheetID)<=1:
+# For defining the defualt sheet_id if it is not given
         sheetID="1A59j3aXKLHkwVx0nj8hMpX8gksnvJ5T2rgZCVNNKxYI"
     
         
